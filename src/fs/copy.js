9dd331191527068ac2f57a1dbcbcfb2e43547361
Tedzury <asdrogachev@gmail.com>
2024-01-26 08:49:50 +0200

    init: solved first two task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import { access, cp } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const srcFolderPath = `${__dirname}/files`;
+const destFolderPath = `${__dirname}/files_copy`;
+const foldersErrText = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+    let doesSrcExist = false;
+    let doesNotDestExist = false;
+    try {
+        await access(srcFolderPath)
+        doesSrcExist = true;
+    } catch {}
+    try {
+        await access(destFolderPath)
+    } catch {
+        doesNotDestExist = true;
+    }
+    try {
+        if (doesSrcExist && doesNotDestExist) {
+            return cp(srcFolderPath, destFolderPath, {recursive: true});
+        }
+        throw new Error(foldersErrText);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import { access, writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = `${__dirname}/files/fresh.txt`;
+const fileContent = 'I am fresh and young';
+const fileExistErrText = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await access(filePath)
+        throw new Error(fileExistErrText);
+    } catch(err) {
+        if (err.message === fileExistErrText) return console.log(err.message)
+        try {
+            writeFile(filePath, fileContent);
+        } catch (err) {
+            console.log('Something went wrong during file write')
+        }
+    }
 };
 
 await create();
